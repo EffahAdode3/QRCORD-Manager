ec8153d6099e015ebfc507fe82ae9e0807638967
EffahAdode3 <whatyousow00@gmail.com>
2025-07-11 23:30:55 +0000

    sfd

--- db/config.js
+++ db/config.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
     dialect: "sqlite",
+    dialectModule: require('better-sqlite3'),
     storage: "./database.sqlite", // This file will be created in your project root
     logging: false
 });
